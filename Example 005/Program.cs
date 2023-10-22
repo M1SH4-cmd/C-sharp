c3045f70e339cd66def6e3cb7d8317e7e48bdd64
M1SH4-cmd <6el0zu61979@mail.ru>
2023-10-22 07:14:25 +0700

    Example 005 User Greeting special

---
+++ Example 005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "og michael")
+{
+    Console.WriteLine("Ayo wassup?");
+}
+else
+{
+    Console.Write("Greetengs, ");
+    Console.Write(username);
+}
