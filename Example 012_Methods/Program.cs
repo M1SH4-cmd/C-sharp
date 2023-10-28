8b24aee4375f4168ec3ed9ac75396145309c6969
M1SH4-cmd <6el0zu61979@mail.ru>
2023-10-28 21:40:33 +0700

    method 21

---
+++ Example 012_Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Вид 1
+// void Method1()
+// {
+//     Console.WriteLine("OG Michael");
+// }
+// Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2(msg: "Text of message")
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Text", 4);
