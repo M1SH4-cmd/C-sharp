28d0ca3d3ade89b5e3d33d01034cfda40a4fa686
M1SH4-cmd <6el0zu61979@mail.ru>
2023-10-28 22:03:35 +0700

    All 4 types of voids

--- Example 012_Methods/Program.cs
+++ Example 012_Methods/Program.cs
@@ -6,19 +6,47 @@
 // Method1();
 
 // Вид 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
 // Method2(msg: "Text of message")
 
-void Method21(string msg, int count)
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg: "Text", count: 4);
+// Method21( count: 4, msg: "New Text");
+
+// Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+
+// int year = Method3();
+// Console.WriteLine(year);
+
+
+// Вид 4
+string Method4(int count, string text)
 {
     int i = 0;
+    string result = string.Empty;
+
     while (i < count)
     {
-        Console.WriteLine(msg);
+        result = result + text;
         i++;
     }
+    return result;
 }
-Method21("Text", 4);
+string res = Method4(10, "z");
+Console.WriteLine(res);
