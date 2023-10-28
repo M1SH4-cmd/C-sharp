03fb5676326d74e7eba2c7e0875116b169d017bd
M1SH4-cmd <6el0zu61979@mail.ru>
2023-10-29 00:51:33 +0700

    PIZDEC

--- Example 012_Methods/Program.cs
+++ Example 012_Methods/Program.cs
@@ -5,7 +5,7 @@
 // }
 // Method1();
 
-// Вид 2
+// // Вид 2
 // void Method2(string msg)
 // {
 //     Console.WriteLine(msg);
@@ -24,7 +24,7 @@
 // Method21(msg: "Text", count: 4);
 // Method21( count: 4, msg: "New Text");
 
-// Вид 3
+// // Вид 3
 
 // int Method3()
 // {
@@ -35,18 +35,129 @@
 // Console.WriteLine(year);
 
 
-// Вид 4
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = string.Empty;
+// // Вид 4
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = string.Empty;
 
-    while (i < count)
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// string Method4(int count, string text)
+// {
+    
+//     string result = string.Empty;
+//     for(int i = 0; i < count; i++)
+   
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "z");
+//Console.WriteLine(res);
+
+
+// for (int i = 2; i < 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =====Работа с текстом
+// Дан текст, в Тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "С".
+
+// Ясна ли задача?
+
+// string text = "- Я думаю, - сказал князь, улыбаясь, -что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде"
+//             + "вы бы взяли приступом согласие прусского короля."
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = string.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
     {
-        result = result + text;
-        i++;
+        Console.Write($"{array[i]} ");
     }
-    return result;
+    Console.WriteLine();
 }
-string res = Method4(10, "z");
-Console.WriteLine(res);
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1 ; i++)
+    {
+        int minPosition = i;
+       
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
