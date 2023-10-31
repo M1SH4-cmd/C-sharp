a24c20a0eeb149fcc0a8497a25622fdc4c590cf0
M1SH4-cmd <6el0zu61979@mail.ru>
2023-10-31 20:42:02 +0700

    Genius move detected

---
+++ HomeTaskLesson1/Practical work 2/Program.cs
@@ -0,0 +1,6 @@
+﻿int first = 23;
+int second = 7;
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n % 23 == 0 && n % 7 == 0) Console.WriteLine($"Число {n} кратно 23 и 7");
+else Console.WriteLine($"Число {n} не кратно 23 и 7");
