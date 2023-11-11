0b8af6bf17544d5ab3ae8acf133b4b65664df16d
M1SH4-cmd <6el0zu61979@mail.ru>
2023-11-11 16:57:12 +0700

    GG

---
+++ Example014_digit_number_summa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите трехзначное число:");
+        
+        // Ввод трехзначного числа
+        string input = Console.ReadLine();
+
+        // Проверка на корректность ввода
+        if (input.Length == 3 && int.TryParse(input, out int number))
+        {
+            // Вычисление суммы цифр
+            int digit1 = number / 100;
+            int digit2 = (number % 100) / 10;
+            int digit3 = number % 10;
+
+            int sum = digit1 + digit2 + digit3;
+
+            // Вывод результата
+            Console.WriteLine($"Сумма цифр введенного числа {number} равна: {sum}");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите трехзначное число.");
+        }
+    }
+}
