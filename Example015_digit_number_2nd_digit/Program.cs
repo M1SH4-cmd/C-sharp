755969131641b2309bea424059a4dba85166da5e
M1SH4-cmd <6el0zu61979@mail.ru>
2023-11-11 16:58:09 +0700

    Digit is EZ

---
+++ Example015_digit_number_2nd_digit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите трёхзначное число:");
+        string input = Console.ReadLine();
+
+        if (IsThreeDigitNumber(input))
+        {
+            char middleDigit = GetMiddleDigit(input);
+            Console.WriteLine($"Число по середине: {middleDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Число не трёхзначное.");
+        }
+    }
+
+    static bool IsThreeDigitNumber(string input)
+    {
+        return input.Length == 3 && int.TryParse(input, out _);
+    }
+
+    static char GetMiddleDigit(string input)
+    {
+        int middleIndex = input.Length / 2;
+        return input[middleIndex];
+    }
+}
